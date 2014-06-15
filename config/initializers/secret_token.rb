ed30aedbcbbe7aadedb66000438f7b6d2550f42b
Lisa Schaefer <rolemodelenterprises@gmail.com>
2014-06-15 18:48:13 -0400

    Pictures table

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Marasite::Application.config.secret_token = '0335f56b3d2efd881a5d715cd25e44e942d1dee4fa7f013f7424cfcac48c9d8266c8f43f8bf5d08a98038481dff53bedd1b8d0627d290bf7f04f2a028cfa6a72'
