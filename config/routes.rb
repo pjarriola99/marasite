d8e5e866a78c32d3881859beccb6ff6ff3c17500
ece106 <lisa@crowdpublish.tv>
2019-07-15 11:37:28 -0700

    Edit example page for Peter

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Marasite::Application.routes.draw do
   root to: "static_pages#home"
 
   match 'home',           to: 'static_pages#home', via: 'get'
+  match 'example',           to: 'static_pages#philrapamusic', via: 'get'
 end
